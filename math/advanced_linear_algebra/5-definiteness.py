8ad65fbccf942ad7c34bd1b68d816bafae8b9788
Islam Muradov Tahir <imuradov18193@ada.edu.az>
2026-02-08 22:55:43 +0400

    Add files via upload

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Definitess"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    Check the definiteness of a matrix
+    """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.size == 0:
+        return None
+    row, col = matrix.shape
+    if not row == col:
+        return None
+    if not np.all(matrix == matrix.T):
+        return None
+
+    eigen_values, _ = np.linalg.eig(matrix)
+
+    min_value = np.min(eigen_values)
+    max_value = np.max(eigen_values)
+
+    if max_value > 0 and min_value > 0:
+        return "Positive definite"
+    elif max_value > 0 and min_value == 0:
+        return "Positive semi-definite"
+    elif max_value < 0 and min_value < 0:
+        return "Negative definite"
+    elif max_value == 0 and min_value < 0:
+        return "Negative semi-definite"
+    elif max_value > 0 > min_value:
+        return "Indefinite"
+    return None
